8c8f9d626282f63840f0d9714a503e8d48de4a52
Stefan Kovarbasic <kovar.zmaj@gmail.com>
2020-07-13 14:01:57 +0200

    Production build

---
+++ build/precache-manifest.bc1a9392b08c70ee837e02921478f682.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e943dcb67271fc07c7e2bedecab283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98640b5a698cd5913252",
+    "url": "/static/css/main.4f049251.chunk.css"
+  },
+  {
+    "revision": "410aa92793134431d979",
+    "url": "/static/js/2.bb00c429.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bb00c429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98640b5a698cd5913252",
+    "url": "/static/js/main.74d58ce9.chunk.js"
+  },
+  {
+    "revision": "9d1d1d68778dbaa484d5",
+    "url": "/static/js/runtime-main.30491727.js"
+  },
+  {
+    "revision": "a4ef11ba65b008d6fdb34c8b1c47319e",
+    "url": "/static/media/box.a4ef11ba.png"
+  },
+  {
+    "revision": "3af6f8d128d766f1727a51694838e65f",
+    "url": "/static/media/call.3af6f8d1.png"
+  },
+  {
+    "revision": "5d2fa82715b6348013a0229fe4821f62",
+    "url": "/static/media/contact.5d2fa827.png"
+  },
+  {
+    "revision": "eb1e7f21c37b7b0af885adc04ed356ed",
+    "url": "/static/media/home.eb1e7f21.png"
+  },
+  {
+    "revision": "8b46be6d4c7efa5f8f040893b6e508bb",
+    "url": "/static/media/location.8b46be6d.png"
+  },
+  {
+    "revision": "3e813bb8d849bc0c2ab1dcbf2273d3f3",
+    "url": "/static/media/logo.3e813bb8.png"
+  },
+  {
+    "revision": "b7ab6a3892df42ad673cfbcf1f662eaf",
+    "url": "/static/media/mail.b7ab6a38.png"
+  },
+  {
+    "revision": "d0714199c07a093969b74fd407ba94a4",
+    "url": "/static/media/money.d0714199.png"
+  },
+  {
+    "revision": "59c2742a582adf841a40abee3343b0f3",
+    "url": "/static/media/nonstop.59c2742a.png"
+  },
+  {
+    "revision": "92281d83adb5b33c640fd76645960fe8",
+    "url": "/static/media/wallpaper.92281d83.jpg"
+  }
+]);
